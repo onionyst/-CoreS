8d1c0eee9aef1178406657088e534f4848e88782
OnionYST <onionyst@126.com>
2019-06-15 15:20:18 +0800

    add stack pressure test

--- user/aslr.c
+++ user/aslr.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+int pressure_fibonacci(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return pressure_fibonacci(n - 1) + pressure_fibonacci(n - 2);
+    }
+}
+
 void print_memory(unsigned char *addr, int size)
 {
     if (size <= 0)
@@ -29,11 +45,16 @@ int main(void)
 {
     int x = 5;
     int y = 10;
-    char *s = "abcdefg";
+    char *s = "ABCDEFG";
 
+    cprintf("====== Stack Top ASLR ======\n");
     cprintf("%p %p %p\n", &x, &y, &s);
-
     print_memory((unsigned char *)(&s - 2), 0x20);
+
+    cprintf("====== Pressure Fibonacci ======\n");
+    cprintf("F[10] = %d\n", pressure_fibonacci(10));
+    cprintf("F[30] = %d\n", pressure_fibonacci(30));
+    cprintf("F[40] = %d\n", pressure_fibonacci(40));
 
     return 0;
 }
